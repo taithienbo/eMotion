b3313065336eadaccf95bfee765cbf973601a26b
Tai Bo <taithienbo@gmail.com>
2012-03-01 00:12:48 -0800

    made an option menu. WHen the menu button is pressed, the settings page shows up

--- ProjectWorkspace/eMotion/src/com/emotion/EMotionActivity.java
+++ ProjectWorkspace/eMotion/src/com/emotion/EMotionActivity.java
@@ -1,13 +1,35 @@
 package com.emotion;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class EMotionActivity extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.option_menu, menu);
+    	return true;
+    }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	
+    	Intent intent = new Intent(EMotionActivity.this, SettingsActivity.class);
+    	startActivity(intent);
+		return true;
+    	
+    }
 }
